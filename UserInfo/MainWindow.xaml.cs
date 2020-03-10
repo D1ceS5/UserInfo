5e299683df8de05476bc2211c6b901c610ddcfd7
D1ceS5 <student@205-06>
2020-03-10 20:25:04 +0200

    накидал говнеца

--- UserInfo/MainWindow.xaml.cs
+++ UserInfo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,24 @@ namespace UserInfo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Init(object sender, RoutedEventArgs e)
+        {
+           string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Remove(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).LastIndexOf("\\"), Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Length - Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).LastIndexOf("\\"));
+            MessageBox.Show(path);
+            foreach (var item in Directory.GetDirectories(path))
+            {
+                UserList.Items.Add(item.Remove(0,item.LastIndexOf('\\')+1));
+            }
+        }
+
+        private void slcchng(object sender, SelectionChangedEventArgs e)
+        {
+            if(UserList.SelectedIndex!=-1)
+            {
+
+            }
         }
     }
 }
